20d59597124c798678c58217ce258a1b3404e8d0
roney ruiz <roneyruizrojas@gmail.com>
2025-01-25 20:54:31 -0600

    learn: structures

---
+++ Src/Core/Structures.cs
@@ -0,0 +1,26 @@
+namespace Src.Core;
+
+public class Structures
+{
+  public static void Run()
+  {
+    List<int> numbers = [1, 2, 3, 4];
+    numbers.ForEach(n => Console.WriteLine(n));
+
+
+    Dictionary<string, int> fruits = new() {
+      {"apple", 1000},
+      {"banana", 9999},
+      {"watermelon", 3333},
+    };
+    foreach (var fruit in fruits) {
+      Console.WriteLine($"Fruit: {fruit.Key}, Price: {fruit.Value}");
+    };
+
+
+    HashSet<int> ids = [1, 2, 3, 2];
+    foreach (var id in ids) {
+      Console.WriteLine($"ID: {id}");
+    };
+  }
+}
