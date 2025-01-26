03e278c904f8179abd4f9280feee0dcbee677c63
roney ruiz <roneyruizrojas@gmail.com>
2025-01-25 20:54:10 -0600

    learn: variables

---
+++ Src/Core/Variables.cs
@@ -0,0 +1,19 @@
+namespace Src.Core;
+
+public class Variables
+{
+  public static void Run()
+  {
+    string text = "random text";
+    Console.WriteLine(text);
+
+    int number = 1234;
+    Console.WriteLine(number);
+
+    bool maybe = true;
+    Console.WriteLine(maybe);
+
+    double pi = 3.14;
+    Console.WriteLine(pi);
+  }
+};
